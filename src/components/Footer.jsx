00914900a3aeb6e514b3ff05216f97119108bfcb
Arild-BM <112947008+Arild-BM@users.noreply.github.com>
2023-01-10 09:41:40 +0100

    Fjernet varsel

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,11 +5,11 @@ export default function Footer(props) {
         <div className={props.darkMode ? "dark": ""}>
             <h2>Utvalgte prosjekter:</h2>
             <div className="projects">
-                <a className={props.darkMode ? "dark": ""} href="https://arild-bm.github.io/Parallax-Website/" target="_blank">Stavanger<br />sentrum</a>
-                <a className={props.darkMode ? "dark": ""} href="https://arild-bm.github.io/Stavanger/" target="_blank">Visit<br />Stavanger</a>
-                <a className={props.darkMode ? "dark": ""} href="https://arild-bm.github.io/Solsystem/" target="_blank">Solar<br />System</a>
-                <a className={props.darkMode ? "dark": ""} href="https://arild-bm.github.io/Rotating-pictures/" target="_blank">Rotating<br />Pictures</a>
-                <a className={props.darkMode ? "dark": ""} href="https://arild-bm.github.io/christmas-task/" target="_blank">Christmas<br />Countdown</a>
+                <a className={props.darkMode ? "dark": ""} href="https://arild-bm.github.io/Parallax-Website/" rel="noreferrer" target="_blank">Stavanger<br />sentrum</a>
+                <a className={props.darkMode ? "dark": ""} href="https://arild-bm.github.io/Stavanger/" rel="noreferrer" target="_blank">Visit<br />Stavanger</a>
+                <a className={props.darkMode ? "dark": ""} href="https://arild-bm.github.io/Solsystem/" rel="noreferrer" target="_blank">Solar<br />System</a>
+                <a className={props.darkMode ? "dark": ""} href="https://arild-bm.github.io/Rotating-pictures/" rel="noreferrer" target="_blank">Rotating<br />Pictures</a>
+                <a className={props.darkMode ? "dark": ""} href="https://arild-bm.github.io/christmas-task/" rel="noreferrer" target="_blank">Christmas<br />Countdown</a>
             </div>
         </div>
     )
