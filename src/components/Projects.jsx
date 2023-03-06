c4a6bc86b0ffdcb5c3492b74e0764238f409309b
Arild-BM <112947008+Arild-BM@users.noreply.github.com>
2023-03-06 14:30:59 +0100

    Rotating text

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -30,13 +30,15 @@ export default function Projects(props) {
             if (change) {
                 setProjectText(text[count])
                 setPicture(pictures[count])
+                document.getElementById(count).focus()
             }
         }, 1500)
         // eslint-disable-next-line
     }, [count]);
 
-    function showImage(image, txt) {
+    function showImage(image, txt, number) {
         change = false;
+        document.getElementById(number).focus()
         setClass2(`${styles.bold}`)
         setPicture(image)
         setProjectText(txt)
@@ -55,49 +57,55 @@ export default function Projects(props) {
             <div className={`${styles.projectsection}`}>
                 <div className={`${styles.projects}`}>
                     <a 
-                        onMouseEnter={() => showImage(Picture1, myData.project1)}
+                        onMouseEnter={() => showImage(Picture1, myData.project1, 0)}
                         onMouseLeave={() => hideImage()}
                         className={props.darkMode ? `dark ${class2}`: `${class2}`}
+                        id = "0"
                         href="https://arild-bm.github.io/Parallax-Website/"
                         rel="noreferrer"
                         target="_blank">Stavanger<br />sentrum
                     </a>
                     <a 
-                        onMouseEnter={() => showImage(Picture2, myData.project2)}
+                        onMouseEnter={() => showImage(Picture2, myData.project2, 1)}
                         onMouseLeave={() => hideImage()}
                         className={props.darkMode ? "dark": ""}
+                        id = "1"
                         href="https://arild-bm.github.io/Stavanger/"
                         rel="noreferrer"
                         target="_blank">Visit<br />Stavanger
                     </a>
                     <a 
-                        onMouseEnter={() => showImage(Picture3, myData.project3)}
+                        onMouseEnter={() => showImage(Picture3, myData.project3, 2)}
                         onMouseLeave={() => hideImage()}
                         className={props.darkMode ? "dark": ""}
+                        id = "2"
                         href="https://arild-bm.github.io/Solsystem/"
                         rel="noreferrer"
                         target="_blank">Solar<br />System
                     </a>
                     <a 
-                        onMouseEnter={() => showImage(Picture4, myData.project4)}
+                        onMouseEnter={() => showImage(Picture4, myData.project4, 3)}
                         onMouseLeave={() => hideImage()}
                         className={props.darkMode ? "dark": ""}
+                        id = "3"
                         href="https://arild-bm.github.io/Rotating-pictures/"
                         rel="noreferrer"
                         target="_blank">Roterende<br />bilder
                     </a>
                     <a  
-                        onMouseEnter={() => showImage(Picture5, myData.project5)}
+                        onMouseEnter={() => showImage(Picture5, myData.project5, 4)}
                         onMouseLeave={() => hideImage()}
                         className={props.darkMode ? "dark": ""}
+                        id = "4"
                         href="https://arild-bm.github.io/christmas-task/"
                         rel="noreferrer"
                         target="_blank">Nedtelling<br />til jul
                     </a>
                     <a  
-                        onMouseEnter={() => showImage(Picture6, myData.project6)}
+                        onMouseEnter={() => showImage(Picture6, myData.project6, 5)}
                         onMouseLeave={() => hideImage()}
                         className={props.darkMode ? "dark": ""}
+                        id = "5"
                         href="https://arild-bm.github.io/quizzical/"
                         rel="noreferrer"
                         target="_blank">Quizzical
